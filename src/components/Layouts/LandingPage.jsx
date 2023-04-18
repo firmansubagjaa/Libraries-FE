1a489c6e6144f9fb0dfd0206487d815db7257228
firmanssssss <avenged.firman33@gmail.com>
2023-04-18 17:18:17 +0700

    fix: add and change landingpage from fragments to main

--- src/components/Layouts/LandingPage.jsx
+++ src/components/Layouts/LandingPage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Navbar from "../Fragments/Navbar";
 import Hero from "../Fragments/Hero";
+import PopularSection from "../Fragments/Popular";
+import LatestBooks from "../Fragments/Latest Books";
+import Subscribe from "../Fragments/Subscribe";
+import Footer from "../Fragments/Footer";
 
 export default function LandingPage() {
   return (
-    <>
+    <main className="relative">
       <Navbar />
       <Hero />
-    </>
+      <PopularSection />
+      <LatestBooks />
+      <Subscribe />
+      <Footer />
+    </main>
   );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import Navbar from "../../components/Fragments/Navbar";
-import Hero from "../../components/Fragments/Hero/index";
-import PopularSection from "../../components/Fragments/Popular";
-import LatestBooks from "../../components/Fragments/Latest Books";
-import Subscribe from "../../components/Fragments/Subscribe";
-import Footer from "../../components/Fragments/Footer";
+import LandingPage from "../../components/Layouts/LandingPage";
 
 export default function Home() {
   return (
-    <main className="relative">
-      <Navbar />
-      <Hero />
-      <PopularSection />
-      <LatestBooks />
-      <Subscribe />
-      <Footer />
-    </main>
+    <>
+      <LandingPage />
+    </>
   );
 }
